05e076a536be65d05c0cc3d75dfb487e490db31c
marcefalb <marcefalb@gmail.com>
2021-12-10 22:49:56 +0300

    formik validation

--- src/methodist/ContactsForm/ContactsForm.jsx
+++ src/methodist/ContactsForm/ContactsForm.jsx
@@ -1,5 +1,6 @@
 import {React} from "react";
-import { Formik, Form } from 'formik';
+import { Formik, Form, ErrorMessage  } from 'formik';
+import * as Yup from "yup";
 
 import InputItem from "./InputItem/InputItem";
 import Button from "components/Form/Button/Button"
@@ -12,6 +13,18 @@ import { observer } from "mobx-react-lite";
 
 const ContactsForm = observer(() => {
   const pageStates = store.pageStates
+  const contactsScheme = Yup.object().shape({
+    name: Yup.string()
+      .required('Введите ваше имя'),
+    surname: Yup.string()
+      .required('Введите вашу фамилию'),
+    secondname: Yup.string()
+      .required('Введите ваше отчество'),
+    city: Yup.string()
+      .required('Введите ваш город'),
+    company: Yup.string()
+      .required('Введите вашу компанию/организацию'),
+  });
 
   if (!pageStates.isRequestFormed || pageStates.isFormSended) return null
   return (
@@ -28,20 +41,30 @@ const ContactsForm = observer(() => {
           city: '',
           company: '',
         }}
+        validationSchema={contactsScheme}
+        onSubmit={() => pageStates.setPageState('isFormSended', true)}
       >
         <Form className="contacts__form">
           <InputItem title="Имя" name="name" placeholder="Иван" />
+          <span className="contacts__validation-message"><ErrorMessage name="name" /></span>
+          
           <InputItem title="Фамилия" name="surname" placeholder="Александров" />
+          <span className="contacts__validation-message"><ErrorMessage name="surname" /></span>
+
           <InputItem title="Отчество" name="secondname" placeholder="Алексеевич" />
+          <span className="contacts__validation-message"><ErrorMessage name="secondname" /></span>
+
           <InputItem title="Город" name="city" placeholder="Москва" />
+          <span className="contacts__validation-message"><ErrorMessage name="city" /></span>
+
           <InputItem title="Компания" name="company" placeholder="МГОК" />
+          <span className="contacts__validation-message"><ErrorMessage name="company" /></span>
           
           <div className="contacts__send-btn">
             <Button
               text="Отправить"
               theme="outlined"
               size="normal"
-              onClick={() => pageStates.setPageState('isFormSended', true)}
             />
           </div>
         </Form>
